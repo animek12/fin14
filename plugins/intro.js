57ce05a4ed631d515c097626cd8de24746899e09
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-21 18:24:49 +0800

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -38,10 +38,10 @@ let krtu = `0ཻུ۪۪ꦽꦼ̷⸙‹•═════════════�
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/al_vin.233",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://linktr.ee/fin222", 
+    title: 'FIN BOTZ MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
