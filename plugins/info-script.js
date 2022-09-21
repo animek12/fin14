54e9c3cff5a174ed06821a7a5369be60d4d9c0ae
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-21 18:20:14 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,26 +2,20 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://linktr.ee/fin222*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
 https://github.com/ImYanXiao/Elaina-MultiDevice
 
-Mau Yang No Error Script Bot nya, Gas Cek Video YT :
-https://youtu.be/Pgv_GuQTPyo
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6282127487538`
+wa.me/6282261065144`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/al_vin.233",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://linktr.ee/fin222", 
+    title: 'FIN BOT MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
